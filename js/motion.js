22a95b74b20252a3a509d7f5c77062a1bc0d118b
bichron <chrispham.pnb@gmail.com>
2026-02-20 00:49:31 +0700

    Update motion.js

--- js/motion.js
+++ js/motion.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded",()=>{
+
 if(window.DeviceOrientationEvent){
 window.addEventListener("deviceorientation",e=>{
 const x=e.gamma/40;
@@ -5,3 +7,5 @@ const y=e.beta/40;
 document.getElementById("phone").style.rotate=`${-y}deg ${x}deg`;
 });
 }
+
+});
