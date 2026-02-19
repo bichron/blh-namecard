b55133e41c2adbfc773ea157d0fac10d7bdea725
bichron <chrispham.pnb@gmail.com>
2026-02-20 01:10:03 +0700

    Delete nfc.js

--- js/nfc.js
+++
@@ -1,7 +0,0 @@
-const params=new URLSearchParams(location.search);
-if(params.has("nfc")){
-document.getElementById("phone").animate(
-[{transform:"scale(.95)"},{transform:"scale(1)"}],
-{duration:800,easing:"ease-out"}
-);
-}
