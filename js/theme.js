ce32490d19a285b7ef6324e5d18206be24093b41
bichron <chrispham.pnb@gmail.com>
2026-02-20 00:38:42 +0700

    Create theme.js

---
+++ js/theme.js
@@ -0,0 +1,11 @@
+function toggleTheme(){
+document.body.classList.toggle("light");
+localStorage.setItem(
+"theme",
+document.body.classList.contains("light") ? "light" : "dark"
+);
+}
+
+if(localStorage.getItem("theme")==="light"){
+document.body.classList.add("light");
+}
