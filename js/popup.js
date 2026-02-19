ce110ad785ed1e3a5f2bb3d53722a4789f127063
bichron <chrispham.pnb@gmail.com>
2026-02-20 00:40:07 +0700

    Create popup.js

---
+++ js/popup.js
@@ -0,0 +1,9 @@
+function openQR(){qrPopup.classList.add("active");}
+function closeQR(){qrPopup.classList.remove("active");}
+
+function openEnterprise(){enterprisePopup.classList.add("active");}
+function closeEnterprise(){enterprisePopup.classList.remove("active");}
+
+function openWebsite(){
+window.open("https://yourwebsite.com","_blank");
+}
