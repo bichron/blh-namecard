51bac659b7d6a47a4611f0c70574836dd22bfc18
bichron <chrispham.pnb@gmail.com>
2026-06-07 23:23:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -271,7 +271,7 @@ Tax Code: 0318787124
 Registered Address:
 2220 Đỗ Mười,
 Phường Trung Mỹ Tây,
-Thành phố Hồ Chí Minh, Vietnam
+Thành phố Hồ Chí Minh, Việt 
 
 Email: support@blh.vn
 Website: https://blh.vn
