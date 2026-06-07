c35a6e7a03bf259948f14eec3e0cb9eb9c0905ce
bichron <chrispham.pnb@gmail.com>
2026-06-07 21:31:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -474,3 +474,44 @@ END:VCARD`;
 };
 
 });
+
+
+function copyCompanyInfo() {
+
+  const text = `BLH JOINT STOCK COMPANY
+
+Tax Code: 0318787124
+
+Registered Address:
+2220 Đỗ Mười,
+Phường Trung Mỹ Tây,
+Thành phố Hồ Chí Minh, Vietnam
+
+Email: support@blh.vn
+Website: https://blh.vn
+
+Business Contact:
+0819091981`;
+
+  navigator.clipboard.writeText(text)
+    .then(() => {
+
+      const stamp = document.querySelector('.enterprise-stamp');
+
+      const oldTitle = stamp.title;
+
+      stamp.title = "Copied ✓";
+
+      stamp.style.transform = "scale(1.1)";
+
+      setTimeout(() => {
+        stamp.title = oldTitle;
+        stamp.style.transform = "";
+      }, 1200);
+
+    })
+    .catch(err => {
+      console.error("Copy failed:", err);
+      alert("Unable to copy information");
+    });
+}
